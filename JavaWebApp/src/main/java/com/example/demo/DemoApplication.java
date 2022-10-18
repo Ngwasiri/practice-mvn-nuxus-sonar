11284e15b69574375cec856247ddf8f419595af8
Home 2 <ngwasirilouis.com>
2022-10-18 19:35:35 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	
+		String name = "Che";
+		System.out.println(name);
 	}
-
 }
